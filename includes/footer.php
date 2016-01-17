fadc6b3520d55ccb697a784f998ca3d762c6dc24
Jose Alvarado <jose_alvarado17@hotmail.com>
2016-01-16 20:37:07 -0430

    Se agregó el boton hacia Github

--- includes/footer.php
+++ includes/footer.php
@@ -1,9 +1,16 @@
 <footer>
 	<div class="container">
 		<div class="row">
-			<div class="col-xs-12">
-				<div class="coding">
-					<i class="fa fa-code"></i> por <a href="#"><i class="fa fa-twitter"></i> @joselavaradoo</a> para la Dirección de Asuntos Estudiantiles. Facultad de Ingeniería. Universidad de Carabobo 2016.
+			<div class="col-xs-11">
+				Dirección de Asuntos Estudiantiles. Facultad de Ingeniería. Universidad de Carabobo 2016.
+				<br>
+				<i class="fa fa-code"></i> por <a href="#"><i class="fa fa-twitter"></i> @joselavaradoo</a>
+			</div>
+			<div class="col-xs-1">
+				<div class="social-media pull-right">
+					<a href="https://github.com/josealvaradoo/dae" target="_blank">
+						<i class="fa fa-github"></i>
+					</a>
 				</div>
 			</div>
 		</div>
